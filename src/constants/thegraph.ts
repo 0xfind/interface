fa872e8425b0f0a94c3ef0889f72a1b3ac39ce9a
nhwu <105170140+nhwu@users.noreply.github.com>
2023-03-11 12:23:59 +0800

    perf: :zap: update UNISWAP_CHAIN_SUBGRAPH_URL for goerli chain

--- src/constants/thegraph.ts
+++ src/constants/thegraph.ts
@@ -26,7 +26,7 @@ export const FIND_CHAIN_SUBGRAPH_URL: Record<number, string> = {
 
 export const UNISWAP_CHAIN_SUBGRAPH_URL: Record<number, string> = {
   [SupportedChainId.GOERLI]:
-    "https://api.thegraph.com/subgraphs/name/0xfind/uniswap-v3-goerli",
+    "https://api.thegraph.com/subgraphs/name/0xfind/uniswap-v3-goerli-2",
   [SupportedChainId.MAINNET]:
     "https://api.thegraph.com/subgraphs/name/0xfind/uniswap-v3-eth",
 };
